5b29b538e7e125a72bf46a0cd52f40bb825e369a
gabriellima <gabriel@test.com>
2022-04-05 11:32:49 -0300

    Criacao do projeto

---
+++ Documents/Aula/pw3/avaliacao/Victor/pw3-detran/src/test/java/br/com/victor/augusto/souza/rocha/detran/Pw3DetranApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.victor.augusto.souza.rocha.detran;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pw3DetranApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
