7a878219b8d6621cb72c7e74b4a9bd351a0948b2
gabriellima <gabriel@test.com>
2022-04-05 12:23:42 -0300

    Criacao da enum EstadoMultaEnum.

---
+++ Documents/Aula/pw3/avaliacao/Victor/detran/detran/src/main/java/br/com/etechoracio/detran/enums/EstadoMultaEnum.java
@@ -0,0 +1,7 @@
+package br.com.etechoracio.detran.enums;
+
+public enum EstadoMultaEnum {
+
+	NO_PRAZO, EM_ATRASO, QUITADA
+	
+}
---
+++ Documents/Aula/pw3/avaliacao/Victor/detran/detran/src/test/java/br/com/etechoracio/detran/DetranApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.etechoracio.detran;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DetranApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
